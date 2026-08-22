0496bee68f0d31ed0920add46016d77f96e27876
Enzo <enzosousasantos08@gmail.com>
2026-08-21 22:14:42 -0300

    adiciona indices e analise de consultas

---
+++ sql/11_indexes.sql
@@ -0,0 +1,12 @@
+SELECT *
+FROM pedidos
+WHERE cliente_id = 1;
+
+CREATE INDEX idx_pedidos_cliente
+ON pedidos(cliente_id);
+
+CREATE INDEX idx_produtos_categoria
+ON produtos(categoria_id);
+
+CREATE INDEX idx_itens_pedido_produto
+ON itens_pedido(produto_id);
